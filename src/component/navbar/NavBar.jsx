abcf0ee1db7b7be1224802bdccb2abbe8c1fcfcf
mars1385 <faceless1385@gmail.com>
2021-06-10 15:01:12 +0430

    init

---
+++ src/component/navbar/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const NavBar = (props) => {
+  return <div></div>;
+};
+
+NavBar.propTypes = {};
+
+export default NavBar;
---
+++ src/component/navbar/Navbar.style.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const navBar = styled.div`
+  width: 100%;
+  height: 50px;
+  background-color: white;
+`;
+
+export const nabBarWrapper = styled.div`
+  height: 100%;
+  padding: 0px 1.4rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+// right side
+export const navBarRightSide = styled.div``;
+
+// left side
+export const navBarLeftSide = styled.div``;
+
+export const navBarLogo = styled.span`
+  font-weight: bold;
+  font-size: 2rem;
+  color: darkgreen;
+  cursor: pointer;
+`;
